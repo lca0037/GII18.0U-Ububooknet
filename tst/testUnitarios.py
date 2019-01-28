773bd94924c346b7b7047da728d5db536b46e4de
Luis Miguel Cabrejas Arce <lca0037@alu.ubu.es>
2019-01-28 13:47:34 +0100

    Se corrigen errores en las pruebas creadas #21
    Se actualizan las pruebas unitarias creadas en el issue #13

--- tst/testUnitarios.py
+++ tst/testUnitarios.py
@@ -5,38 +5,46 @@ Test unitarios sobre el código implementado
 @author: Luis Miguel Cabrejas Arce
 """
 
+
+import sys
+sys.path.append('../src/')
 import unittest
 import modelo as mod
 
-m = mod.modelo();
-m.leerEpub('epubPruebas.epub')
 
+
+#print(sys.path)
+m = mod.modelo();
+m.crearDiccionario()
 class testUnitarios(unittest.TestCase):
     
     def testLecturaEpub(self):
-        res = [['Pedro Pérez', 2], ['Josema',1], ['Pedro',1], ['Pedro Rodríguez',1], ['Ana',1]]
+        res = {0:{'Pedro Pérez': 2}, 1:{'Josema':1}, 2:{'Pedro':2}, 3:{'Pedro Rodríguez':1}, 4:{'Ana':1}}
         self.comprobarPersonajes(res)
     
     def testAnadirPersonaje(self):
         m.anadirPersonaje('Andrea')
-        res = [['Pedro Pérez', 2], ['Josema',1], ['Pedro',1], ['Pedro Rodríguez',1], ['Ana',1], ['Andrea',0]]
+        res = {0:{'Pedro Pérez': 2}, 1:{'Josema':1}, 2:{'Pedro':2}, 3:{'Pedro Rodríguez':1}, 4:{'Ana':1}, 5:{'Andrea':0}}
         self.comprobarPersonajes(res)
         
     def testEliminarPersonaje(self):
         m.eliminarPersonaje(1)
-        res = [['Pedro Pérez', 2], ['Pedro',1], ['Pedro Rodríguez',1], ['Ana',1], ['Andrea',0]]
+        res = {0:{'Pedro Pérez': 2}, 2:{'Pedro':2}, 3:{'Pedro Rodríguez':1}, 4:{'Ana':1}, 5:{'Andrea':0}}
         self.comprobarPersonajes(res)
             
     def testJuntarPersonajes(self):
         m.juntarPersonajes(3,2)
-        res = [['Pedro Pérez', 2], ['Pedro Rodríguez',1], ['Pedro',1], ['Ana',1], ['Andrea',0]]
+        res = {0:{'Pedro Pérez': 2}, 3:{'Pedro Rodríguez':1, 'Pedro':2}, 4:{'Ana':1}, 5:{'Andrea':0}}
         self.comprobarPersonajes(res)
         
     def comprobarPersonajes(self, res):
         obt = m.getPersonajes()
         i = 0
-        for k in obt.keys():
-            for sk in obt[k].keys():
-                self.assertEqual(sk,res[i][0])
-                self.assertEqual(obt[k][sk],res[i][1])
+        for k in res.keys():
+            per = obt[k].getPersonaje()
+            for sk in res[k].keys():
+                self.assertEqual(per[sk],res[k][sk])
             i+=1
+            
+if __name__ == '__main__':
+    unittest.main()
